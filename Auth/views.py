abd806b2fdea238641be9c482136e03bf7295d07
Vijay Kanna <vjkanna24@gmail.com>
2025-09-25 12:41:13 +0530

    Handle Logout

--- Auth/views.py
+++ Auth/views.py
@@ -133,18 +133,9 @@ def UserLogOut(request):
         "Status": "Logged Out Successfully",
         "Message": "Data Cleared"
     })
-    response.delete_cookie(
-        ACCESS_TOKEN,
-        httponly=True,
-        samesite="None",
-        secure=True
-    )
-    response.delete_cookie(
-        REFRESH_TOKEN,
-        httponly=True,
-        samesite="None",
-        secure=True
-    )
+    response.delete_cookie(ACCESS_TOKEN, path="/", samesite="None", secure=True)
+    response.delete_cookie(REFRESH_TOKEN, path="/", samesite="None", secure=True)
+
     return response
 
 
